37acf0cd93c44479e6d1d200d9598a14fc127215
leehyewon0531 <ifpffk@naver.com>
2023-10-30 12:32:43 +0900

    RAC-89 fix: login.tsx, page.tsx 발생 오류 해결

--- src/app/login/oauth2/code/kakao/page.tsx
+++ src/app/login/oauth2/code/kakao/page.tsx
@@ -1,63 +1,48 @@
-
-"use client";
+'use client'
 import React from 'react'
-import { useEffect ,useState} from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { useAccessToken } from '@/context/context';
-import { useCookies } from 'react-cookie';
+import { useEffect, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useCookies } from 'react-cookie'
 
 function page() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-    
-    const pathname = usePathname();
-    const router = useRouter();
-    const { provideAccessToken } = useAccessToken();
-    const [cookies, setCookie] = useCookies(['kakao_refreshToken']);
-    const [loginSuccess, setLoginSuccess] = useState(false);
-  
-    useEffect(() => {
+  const pathname = usePathname()
+  const router = useRouter()
+  const [cookies, setCookie] = useCookies(['kakao_refreshToken'])
+  const [loginSuccess, setLoginSuccess] = useState(false)
 
-        const params = new URLSearchParams();
-        params.append("grant_type", "authorization_code");
-        params.append("client_id", process.env.NEXT_PUBLIC_REST_API_KEY);
-        params.append("redirect_uri", process.env.NEXT_PUBLIC_REDIRECT_URI);
-        params.append("code", code);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const code = urlParams.get('code')
 
-        fetch(`https://kauth.kakao.com/oauth/token`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-              },
+    const params = new URLSearchParams()
+    params.append('grant_type', 'authorization_code')
+    params.append('client_id', process.env.NEXT_PUBLIC_REST_API_KEY!)
+    params.append('redirect_uri', process.env.NEXT_PUBLIC_REDIRECT_URI!)
+    params.append('code', code ? code : '')
 
-              body: params,
-          })
-        .then(response => response.json())
-        .then(data => {
-            const kakao_accessToken = data.access_token;
-            const kakao_refreshToken = data.refresh_token;
-            console.log(data.refresh_token)
-            setCookie('kakao_refreshToken', kakao_refreshToken, { path: '/' });
+    fetch(`https://kauth.kakao.com/oauth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
 
-            localStorage.setItem('kakao_access_token', kakao_accessToken);
-            router.replace('/');
-            
-          })
-          .catch(error => {
-            console.error('액세스 토큰 요청 실패:', error);
-          });
-          
-        
-      }, []);
+      body: params,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const kakao_accessToken = data.access_token
+        const kakao_refreshToken = data.refresh_token
+        setCookie('kakao_refreshToken', kakao_refreshToken, { path: '/' })
 
-  return (
-    <div>
-      로그인 중입니다.
-    </div>
-  )
+        localStorage.setItem('kakao_access_token', kakao_accessToken)
+        router.replace('/')
+      })
+      .catch((error) => {
+        console.error('액세스 토큰 요청 실패:', error)
+      })
+  }, [])
+
+  return <div>로그인 중입니다.</div>
 }
 
 export default page
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,33 @@
-"use client";
+'use client'
 import Login from '@/app/components/kakao/login'
 import axios from 'axios'
-import { useEffect,useState } from 'react'
-import { useAccessToken } from '@/context/context';
-import { AccessTokenProvider } from '@/context/context';
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-
-   const [kakaoToken, setKakaoToken] = useState(localStorage.getItem('kakao_access_token')); 
-
+  const [kakaoToken, setKakaoToken] = useState<string | null>(null)
 
   useEffect(() => {
-    const tokenFromLocalStorage = localStorage.getItem('kakao_access_token');
-    setKakaoToken(tokenFromLocalStorage);
-
-  }, [kakaoToken]);
+    const tokenFromLocalStorage = localStorage.getItem('kakao_access_token')
+    setKakaoToken(tokenFromLocalStorage)
+  }, [])
 
   useEffect(() => {
-    if (kakaoToken) { 
-      axios.post('/user/token', {
-        kakaoToken
-      })
-      .then(function (response) {
-        
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    if (kakaoToken) {
+      axios
+        .post('/user/token', {
+          kakaoToken,
+        })
+        .then(function (response) {})
+        .catch(function (error) {
+          console.log(error)
+        })
     }
-  }, [kakaoToken]);
+  }, [kakaoToken])
 
   return (
-
     <div>
-      <AccessTokenProvider>
-        기본 루트 페이지 입니다
-        <Login/>
-      </AccessTokenProvider>
+      기본 루트 페이지 입니다
+      <Login />
     </div>
   )
 }
