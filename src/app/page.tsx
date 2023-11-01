3533f64770f38ddd66170139257dcc5e56cf1479
yeonssu21 <kimys9295@naver.com>
2023-11-01 10:13:40 +0900

    RAC-71 fix: 콘솔문 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,12 @@ export default function Home() {
           accessToken: kakaoToken,
         })
         .then((data) => {
-          console.log(data)
           const code = data.data.code
           if (code === 200) {
-            console.log('유저입니다')
             const accessToken = data.data.data.accessToken
             const refreshToken = data.data.data.refreshToken
             setSeverAccess(accessToken)
           } else if (code === 404) {
-            console.log('유저가 아닙니다')
             const socialId = data.data.data.socialId
             console.log(socialId)
           }
