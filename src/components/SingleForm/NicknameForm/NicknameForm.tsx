7768461a9fd499e8b012d65e3077bede2ab86a79
leehyewon0531 <ifpffk@naver.com>
2023-11-06 19:14:53 +0900

    RAC-75 feat: 영어, 한글 제외 문자 입력시 삭제 함수 구현

--- src/components/SingleForm/NicknameForm/NicknameForm.tsx
+++ src/components/SingleForm/NicknameForm/NicknameForm.tsx
@@ -1,14 +1,32 @@
 'use client';
 import { useAtom } from "jotai";
 import { nickname } from '@/stores/nickname';
+import { useEffect } from 'react';
 
 function NicknameForm() {
   const [userNick, useUserNick] = useAtom(nickname);
-  console.log('userNick is '+userNick);
+
+  function checkNickname(e: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.currentTarget.value;
+    e.currentTarget.value = filterInputText(inputValue);
+    useUserNick(e.currentTarget.value);
+  }
+
+  useEffect(() => {
+    console.log(userNick);
+  }, [userNick]);
+
+  function filterInputText(inputValue: string) {
+    const regex = /^[a-zA-Z가-힣]*$/;
+    if (!regex.test(inputValue)) {
+      return inputValue.replace(/[^a-zA-Z가-힣]/g, '');
+    }
+    return inputValue;
+  }
 
   return(
     <div>
-      <input type="text" name="user-nickname" id="user-nickname" placeholder="닉네임을 입력해주세요." />
+      <input type="text" name="user-nickname" id="user-nickname" placeholder="닉네임을 입력해주세요." onChange={(e) => checkNickname(e)} />
       <button>중복확인</button>
     </div>
   )
