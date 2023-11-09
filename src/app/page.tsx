9686df864fd330066425cf005bf889396241463c
yeonssu21 <kimys9295@naver.com>
2023-11-09 10:58:15 +0900

    RAC-76 fix: 루트페이지에서 테스트컴포넌트 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Login from '@/components/kakao/login';
-import ServiceCondition from '@/components/Termsofservice/ServiceCondition';
+import ServiceCondition from '@/components/Termsofservice/serviceCondition';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import {
@@ -48,7 +48,6 @@ export default function Home() {
     <SeverAccessProvider>
       기본 루트 페이지 입니다
       <Login />
-      <ServiceCondition />
     </SeverAccessProvider>
   );
 }
