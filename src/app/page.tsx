83eb9b41ac20b00b71d0801afc7d186344607082
leehyewon0531 <ifpffk@naver.com>
2023-11-09 17:43:29 +0900

    RAC-108 fix: 카카오 인가코드 로직으로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,35 +11,35 @@ import {
 } from '@/context/SeverAccessProvider';
 
 export default function Home() {
-  const [kakaoToken, setKakaoToken] = useState<string | null>(null);
-  const { kakaoAccess } = useKakaoAccess();
-  const { setSeverAccess } = useSeverAccess();
-  useEffect(() => {
-    setKakaoToken(kakaoAccess);
-  }, []);
+  // const [kakaoToken, setKakaoToken] = useState<string | null>(null);
+  // const { kakaoAccess } = useKakaoAccess();
+  // const { setSeverAccess } = useSeverAccess();
+  // useEffect(() => {
+  //   setKakaoToken(kakaoAccess);
+  // }, []);
 
-  useEffect(() => {
-    if (kakaoToken) {
-      axios
-        .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login`, {
-          accessToken: kakaoToken,
-        })
-        .then((data) => {
-          const code = data.data.code;
-          if (code === 200) {
-            const accessToken = data.data.data.accessToken;
-            const refreshToken = data.data.data.refreshToken;
-            setSeverAccess(accessToken);
-          } else if (code === 404) {
-            const socialId = data.data.data.socialId;
-            console.log(socialId);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-  }, [kakaoToken]);
+  // useEffect(() => {
+  //   if (kakaoToken) {
+  //     axios
+  //       .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login`, {
+  //         accessToken: kakaoToken,
+  //       })
+  //       .then((data) => {
+  //         const code = data.data.code;
+  //         if (code === 200) {
+  //           const accessToken = data.data.data.accessToken;
+  //           const refreshToken = data.data.data.refreshToken;
+  //           setSeverAccess(accessToken);
+  //         } else if (code === 404) {
+  //           const socialId = data.data.data.socialId;
+  //           console.log(socialId);
+  //         }
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       });
+  //   }
+  // }, [kakaoToken]);
 
   return (
     <SeverAccessProvider>
