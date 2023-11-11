b3d44403c4a5956aebc91147717adf4d3a03a10e
leehyewon0531 <ifpffk@naver.com>
2023-11-11 17:36:43 +0900

    RAC-108 feat: 전화번호 유효성 검사 구현

--- src/components/SingleForm/PhoneNumForm/PhoneNumForm.tsx
+++ src/components/SingleForm/PhoneNumForm/PhoneNumForm.tsx
@@ -1,22 +1,53 @@
 'use client';
 import SingleValidator from "@/components/Validator/SingleValidator";
 import { PhoneNumContainer } from "./PhoneNumForm.styled";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PhoneNumForm() {
   const [flag, setFlag] = useState(false); // 최초 입력 체크하는 flag
+  let maxLength = 0;
 
-  
+  function checkLength(inputValue: string, maxLength: number) {
+    return inputValue.slice(0, maxLength);
+  }
+
+  function filterInputNum(inputValue: string) {
+    const filteredValue = inputValue.replace(/[^0-9]/g, '');
+    return filteredValue;
+  }
+
+  function checkPhoneNum(e: React.ChangeEvent<HTMLInputElement>) {
+    if(!flag) setFlag(true);
+    maxLength = (e.currentTarget.id.includes('fir') ? 3 : 4);
+    e.currentTarget.value = filterInputNum(e.currentTarget.value);
+    e.currentTarget.value = checkLength(e.currentTarget.value, maxLength);
+    if(checkValidation()) setFlag(false);
+  }
+
+  function checkValidation() {
+    let isValid = true;
+    if(typeof document !== undefined) {
+      const firInput = document.getElementById('phone-num-input-fir') as HTMLInputElement;
+      const secInput = document.getElementById('phone-num-input-sec') as HTMLInputElement;
+      const thiInput = document.getElementById('phone-num-input-thi') as HTMLInputElement;
+
+      if(firInput.value.length < 3) isValid = false;
+      if(secInput.value.length < 4) isValid = false;
+      if(thiInput.value.length < 4) isValid = false;
+    }
+
+    return isValid;
+  }
 
   return (
     <div>
       <h3>휴대번호를 입력해주세요</h3>
       <PhoneNumContainer>
-        <input type="text" id="phone-num-input-fir" className="phone-num-input" />
+        <input type="text" id="phone-num-input-fir" className="phone-num-input" onChange={(e) => checkPhoneNum(e)} />
         <div>-</div>
-        <input type="text" id="phone-num-input-sec" className="phone-num-input" />
+        <input type="text" id="phone-num-input-sec" className="phone-num-input" onChange={(e) => checkPhoneNum(e)} />
         <div>-</div>
-        <input type="text" id="phone-num-input-thi" className="phone-num-input" />
+        <input type="text" id="phone-num-input-thi" className="phone-num-input" onChange={(e) => checkPhoneNum(e)} />
       </PhoneNumContainer>
       {flag && <SingleValidator textColor='#FF3347' msg="010-0000-0000의 형식으로 입력해주세요" />}
     </div>
