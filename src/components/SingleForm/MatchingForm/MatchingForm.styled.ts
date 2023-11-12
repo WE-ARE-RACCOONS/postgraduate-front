a38a38f440e7f100b98095e6fa27569bf7264751
leehyewon0531 <ifpffk@naver.com>
2023-11-12 16:07:12 +0900

    RAC-135 style: MatchingForm 기본 스타일 세팅

---
+++ src/components/SingleForm/MatchingForm/MatchingForm.styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const MatchingFormContainer = styled.div`
+  width: 20.5rem;
+
+  #matching-info-form {
+    width: 20.5rem;
+  }
+`
+
+export const MatchingFormHeader = styled.div`
+  width: 20.5rem;
+  display: flex;
+  justify-content: space-between;
+
+  #matching-form-char-count {
+    font-size: 0.94rem;
+  }
+`
+
+export const MatchingFormTitle = styled.div`
+  width: max-content;
+  height: 1.375rem;
+  display: flex;
+
+  #matching-form-title {
+    font-weight: 600;
+  }
+`
--- src/components/SingleForm/MatchingForm/MatchingForm.tsx
+++ src/components/SingleForm/MatchingForm/MatchingForm.tsx
@@ -1,11 +1,21 @@
+'use client';
+import { MatchingFormContainer, MatchingFormHeader, MatchingFormTitle } from "./MatchingForm.styled";
+
 function MatchingForm() {
   return(
-    <div>
-      <div>희망 대학원/학과</div>
-      <div>(선택)</div>
-      <div>0/50자</div>
-      <textarea name="matching-info" id="matching-info-form" placeholder={`test\ntest2\ntest3`}></textarea>
-    </div>
+    <MatchingFormContainer>
+      <MatchingFormHeader>
+        <MatchingFormTitle>
+          <div id="matching-form-title">희망 대학원/학과</div>
+          <div id="matching-form-isrequired">(선택)</div>
+        </MatchingFormTitle>
+        <div id="matching-form-char-count">0/50자</div>
+      </MatchingFormHeader>
+      <textarea 
+        name="matching-info" 
+        id="matching-info-form" 
+        placeholder={`예시)\n연세대학원 / 컴퓨터과학과\n카이스트 대학원 / 생명화학공학과`}></textarea>
+    </MatchingFormContainer>
   )
 }
 
