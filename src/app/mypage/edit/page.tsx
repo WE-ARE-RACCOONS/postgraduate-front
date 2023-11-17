61a7838404a7e4208f93895b8cab11f15a40b1ea
yeonssu21 <kimys9295@naver.com>
2023-11-17 20:31:21 +0900

    RAC-147 feat: 내정보 수정 누르면 페이지 이동

---
+++ src/app/mypage/edit/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>마이페이지 수정</div>;
+}
+
+export default page;
--- src/components/Profile/ProfileManage/ProfileManage.tsx
+++ src/components/Profile/ProfileManage/ProfileManage.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { ProfileManageBox } from './ProfileManage.styled';
+import { useRouter } from 'next/navigation';
 
 import ContentComponent from '../Box/ContentBox/ContentBox';
 import TitleComponent from '../Box/TitleBox/TitleBox';
 function ProfileManage() {
+  const router = useRouter();
+  const handleProfileEditClick = () => {
+    router.push('/mypage/edit');
+  };
   return (
     <ProfileManageBox>
       <TitleComponent title="회원 상태 변경" />
-      <ContentComponent content="내 정보 수정" />
+      <ContentComponent
+        content="내 정보 수정"
+        onClick={handleProfileEditClick}
+      />
       <ContentComponent content="대학원선배 회원으로 변경" />
     </ProfileManageBox>
   );
