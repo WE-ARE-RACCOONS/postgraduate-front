1c4795618e1c61ba49272e0ff6cb79b09a31f054
yeonssu21 <kimys9295@naver.com>
2023-11-17 20:20:33 +0900

    RAC-147 chore: format fix

--- src/components/Profile/ProfileStateChange/CustomerCenter.tsx
+++ src/components/Profile/ProfileStateChange/CustomerCenter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TitleComponent from '../Box/TitleBox/TitleBox';
 import ContentComponent from '../Box/ContentBox/ContentBox';
-import {  CustomerCenterBox } from './CustomerCenter.styled';
+import { CustomerCenterBox } from './CustomerCenter.styled';
 function CustomerCenter() {
   return (
     <CustomerCenterBox>
