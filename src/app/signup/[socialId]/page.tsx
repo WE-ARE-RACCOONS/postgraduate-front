c2087acafced071c9bce3e2b4088958e0b7c240f
leehyewon0531 <ifpffk@naver.com>
2023-11-20 00:57:41 +0900

    RAC-121 style: 후배/선배 아이콘 가로 정렬

--- src/app/signup/[socialId]/page.tsx
+++ src/app/signup/[socialId]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import TypeBtn from "@/components/Button/TypeBtn";
 import { useRouter, usePathname } from "next/navigation";
+import styled from "styled-components";
 
 function SignUpPage() {
   const router = useRouter();
@@ -13,13 +14,19 @@ function SignUpPage() {
         가입하시려는 회원의 유형을 선택해주세요. <br />
         한쪽을 선택해도, 이후 마이페이지에서 전환 가능해요.
       </div>
-      <div>
+      <TypeBtnWrapper>
         <TypeBtn iconText="후배 아이콘" typeDesc={`멘토링을 받는\n후배 회원가입`} userType="junior" />
         <TypeBtn iconText="선배 아이콘" typeDesc={`멘토링을 진행하는\n대학원 선배 회원가입`} userType="senior" />
-      </div>
+      </TypeBtnWrapper>
       <button onClick={() => {router.push(currentPath + '/common-info')}}>다음으로</button>
     </div>
   );
 }
 
 export default SignUpPage;
+
+const TypeBtnWrapper = styled.div`
+  width: 20.75rem;
+  height: 14rem;
+  display: flex;
+`
