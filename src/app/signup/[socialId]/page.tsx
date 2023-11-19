8be4d3cac83d8ed1ec40624dc3c04e1dc54fd9f2
leehyewon0531 <ifpffk@naver.com>
2023-11-19 23:51:43 +0900

    RAC-121 fix: 회원 유형 선택 페이지 추가하여 라우팅 수정

--- src/app/signup/[socialId]/page.tsx
+++ src/app/signup/[socialId]/page.tsx
@@ -1,15 +1,14 @@
-import NicknameForm from '@/components/SingleForm/NicknameForm';
-import ServiceCondition from '@/components/ServiceCondition';
-import NextBtn from '@/components/Button/NextBtn';
-import PhoneNumForm from '@/components/SingleForm/PhoneNumForm';
+'use client';
+import { useRouter, usePathname } from "next/navigation";
 
 function SignUpPage() {
+  const router = useRouter();
+  const currentPath = usePathname();
+
   return (
     <div>
-      <NicknameForm />
-      <PhoneNumForm />
-      <ServiceCondition />
-      <NextBtn kind="next" url="/matching-info" />
+      <div>회원 유형 선택</div>
+      <button onClick={() => {router.push(currentPath + '/common-info')}}>다음으로</button>
     </div>
   );
 }
