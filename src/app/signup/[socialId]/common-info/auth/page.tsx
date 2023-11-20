43405adbf75a3adc2175a73bdc73c8b1bc818cf0
leehyewon0531 <ifpffk@naver.com>
2023-11-20 14:33:02 +0900

    RAC-122 후배/선배 공통 정보 이후 라우팅 분리

---
+++ src/app/signup/[socialId]/common-info/auth/page.tsx
@@ -0,0 +1,7 @@
+function AuthPage() {
+  return(
+    <div>대학원생 인증 페이지</div>
+  )
+}
+
+export default AuthPage;
--- src/app/signup/[socialId]/common-info/page.tsx
+++ src/app/signup/[socialId]/common-info/page.tsx
@@ -1,15 +1,20 @@
+'use client';
 import NicknameForm from '@/components/SingleForm/NicknameForm';
 import ServiceCondition from '@/components/ServiceCondition';
 import NextBtn from '@/components/Button/NextBtn';
 import PhoneNumForm from '@/components/SingleForm/PhoneNumForm';
+import { useAtomValue } from 'jotai';
+import { userTypeAtom } from '@/stores/signup';
 
 function CommonInfoPage() {
+  const userType = useAtomValue(userTypeAtom);
+
   return (
     <div>
       <NicknameForm />
       <PhoneNumForm />
       <ServiceCondition />
-      <NextBtn kind="next" url="/matching-info" />
+      <NextBtn kind="next" url={userType == 'junior' ? "/matching-info" : "/auth"} />
     </div>
   );
 }
