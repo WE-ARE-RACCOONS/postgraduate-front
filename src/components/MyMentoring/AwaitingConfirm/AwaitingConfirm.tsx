d05aeb1d62d4dd9830b1f1fef6aee84f57c5b52e
yeonssu21 <kimys9295@naver.com>
2023-11-23 11:13:37 +0900

    RAC-178 feat: 확정 대기 컴포넌트 생성

---
+++ src/components/MyMentoring/AwaitingConfirm/AwaitingConfirm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AwaitingConfirm() {
+  return (
+    <div>
+      확정대기 내용 여기다가
+    </div>
+  )
+}
+
+export default AwaitingConfirm
--- src/components/TapBar/TapBar.tsx
+++ src/components/TapBar/TapBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import {TapStyle} from './TapBar.styled';
+import AwaitingConfirm from '../MyMentoring/AwaitingConfirm/AwaitingConfirm';
 
 function TapBar() {
     const [activeTab, setActiveTab] = useState(0);
@@ -12,7 +13,7 @@ function TapBar() {
       const renderTabContent = () => {
         switch (activeTab) {
           case 0:
-            return <div>확정 대기 컴포넌트</div>;
+            return <AwaitingConfirm/>;
           case 1:
             return <div>진행 예정 컴포넌트</div>;
           case 2:
