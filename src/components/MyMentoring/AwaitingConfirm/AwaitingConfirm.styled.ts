da54e001a847a40aab4d7bcb68c28366b7754189
yeonssu21 <kimys9295@naver.com>
2023-11-23 11:30:08 +0900

    RAC-178 feat: AwaitingConfirm 컴포넌트 ui 제작

---
+++ src/components/MyMentoring/AwaitingConfirm/AwaitingConfirm.styled.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const AwaitingConfirmBox = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 12rem;
+`;
+export const AwaitingConfirmContent = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 7.6rem;
+  display: flex;
+`;
+export const AwaitingConfirmProfile = styled.div`
+  border: 1px solid black;
+  width: 4.8rem;
+  height: 4.8rem;
+`;
+export const ConfirmTitle = styled.div`
+  border: 1px solid black;
+`;
+export const UserInfo = styled.div`
+  border: 1px solid black;
+`;
+export const AwaitingConfirmInfo = styled.div`
+  border: 1px solid black;
+`;
+export const AwaitingConfirmState = styled.div`
+  border: 1px solid black;
+
+`;
+export const AwaitingConfirmShow = styled.div`
+  border: 1px solid black;
+
+`;
--- src/components/MyMentoring/AwaitingConfirm/AwaitingConfirm.tsx
+++ src/components/MyMentoring/AwaitingConfirm/AwaitingConfirm.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import{
+    AwaitingConfirmBox,
+    AwaitingConfirmProfile,
+    AwaitingConfirmInfo,
+    AwaitingConfirmState,
+    AwaitingConfirmContent,
+    ConfirmTitle,
+    UserInfo,
+    AwaitingConfirmShow
+} from './AwaitingConfirm.styled';
 
 function AwaitingConfirm() {
   return (
     <div>
-      확정대기 내용 여기다가
+      <AwaitingConfirmBox>
+        <AwaitingConfirmContent>
+            <AwaitingConfirmProfile></AwaitingConfirmProfile>
+            <AwaitingConfirmInfo>
+                <ConfirmTitle>000선배와 멘토링</ConfirmTitle>
+                <UserInfo>대학원 | 학과</UserInfo>
+            </AwaitingConfirmInfo>
+            <AwaitingConfirmState>40분</AwaitingConfirmState>
+        </AwaitingConfirmContent>
+        <AwaitingConfirmShow>신청서 보기</AwaitingConfirmShow>
+      </AwaitingConfirmBox>
     </div>
   )
 }
