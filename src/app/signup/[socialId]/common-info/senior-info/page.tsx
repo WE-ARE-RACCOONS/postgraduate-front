8cf007125d911ffbb28320108db2a97e0a2b108b
leehyewon0531 <ifpffk@naver.com>
2023-11-24 01:05:38 +0900

    RAC-159 chore: senior-info 페이지에 완료 버튼 추가

--- src/app/signup/[socialId]/common-info/senior-info/page.tsx
+++ src/app/signup/[socialId]/common-info/senior-info/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import ModalBtn from "@/components/Button/ModalBtn";
+import NextBtn from "@/components/Button/NextBtn";
 import RiseUpModal from "@/components/Modal/RiseUpModal";
 import useModal from "@/hooks/useModal";
 import { createPortal } from "react-dom";
@@ -19,6 +20,8 @@ function SeniorInfoPage() {
         <ModalBtn btnText="지도 교수님*" modalHandler={modalHandler} />
         <ModalBtn btnText="연구분야*" modalHandler={modalHandler} />
         <ModalBtn btnText="연구 주제 키워드*" modalHandler={modalHandler} />
+        {/* 입력 조건 만족했을 때 넘어가는 걸로 NextBtn 클릭 이벤트 추가 */}
+        <NextBtn kind="route" url="/signup/done" btnText="완료" />
       </BtnContainer>
       {modal && portalElement
         ? createPortal(<RiseUpModal modalHandler={modalHandler} />, portalElement)
