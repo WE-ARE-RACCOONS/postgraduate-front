837f8b3ac6acf1af6699b6d641b070dacd8b796d
leehyewon0531 <ifpffk@naver.com>
2023-11-24 12:21:36 +0900

    RAC-159 chore: MUI 관련 라이브러리 설치

--- src/components/Modal/RiseUpModal/RiseUpModal.tsx
+++ src/components/Modal/RiseUpModal/RiseUpModal.tsx
@@ -1,10 +1,13 @@
+import { TextField } from "@mui/material";
 import { ModalBackground } from "./RiseUpModal.styled";
 
 function RiseUpModal({ modalHandler } : { modalHandler: () => void }) {
 
   return(
     <ModalBackground onClick={modalHandler}>
-      <div className='rise-up-modal'>Modal</div>
+      <div className='rise-up-modal'>
+        <TextField />
+      </div>
     </ModalBackground>
   )
 }
