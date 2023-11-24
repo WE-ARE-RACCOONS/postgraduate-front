ded1541063842be8ec2034305aba5a7ad8c3d509
leehyewon0531 <ifpffk@naver.com>
2023-11-24 12:33:54 +0900

    RAC-159 style: TextField 기본 UI 잡기

--- src/components/Modal/RiseUpModal/RiseUpModal.styled.ts
+++ src/components/Modal/RiseUpModal/RiseUpModal.styled.ts
@@ -35,3 +35,12 @@ export const ModalBackground = styled.div`
     background-color: #fff;
   }
 `
+
+export const TextFieldWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  position: absolute;
+  top: 3.75rem;
+  left: 50%;
+  transform: translateX(-50%);
+`
--- src/components/Modal/RiseUpModal/RiseUpModal.tsx
+++ src/components/Modal/RiseUpModal/RiseUpModal.tsx
@@ -1,12 +1,14 @@
 import { TextField } from "@mui/material";
-import { ModalBackground } from "./RiseUpModal.styled";
+import { ModalBackground, TextFieldWrapper } from "./RiseUpModal.styled";
 
 function RiseUpModal({ modalHandler } : { modalHandler: () => void }) {
 
   return(
     <ModalBackground onClick={modalHandler}>
-      <div className='rise-up-modal'>
-        <TextField />
+      <div className='rise-up-modal' onClick={(e) => e.stopPropagation()}>
+        <TextFieldWrapper>
+          <TextField id="outlined-basic" label="대학원명" variant="outlined" size="small" style = {{width: '18.5rem'}} />
+        </TextFieldWrapper>
       </div>
     </ModalBackground>
   )
