badcb057ee61b0ac281910dc1c316b3eb9d5f31f
leehyewon0531 <ifpffk@naver.com>
2023-11-26 17:36:07 +0900

    RAC-159 chore: useAtom -> useSetAtom으로 변경

--- src/components/SingleForm/TextForm/TextForm.tsx
+++ src/components/SingleForm/TextForm/TextForm.tsx
@@ -1,10 +1,10 @@
 import { TextFormProps } from "@/types/form/textForm";
 import { TextFormEl } from "./TextForm.styled";
-import { useAtom } from "jotai";
+import { useSetAtom } from "jotai";
 import { sLabAtom, sProfessorAtom } from "@/stores/senior";
 
 function TextForm(props: TextFormProps) {
-  const [targetAtom, setTargetAtom] = useAtom(props.targetAtom == 'lab' ? sLabAtom : sProfessorAtom);
+  const setTargetAtom = useSetAtom(props.targetAtom == 'lab' ? sLabAtom : sProfessorAtom);
 
   return(
     <TextFormEl type="text" placeholder={props.placeholder} onChange={(e) => setTargetAtom(e.currentTarget.value)} />
