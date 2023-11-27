c1b9097e9cfe3b6965dd974f39ffdb42fc99c5e8
leehyewon0531 <ifpffk@naver.com>
2023-11-27 22:37:32 +0900

    RAC-159 style: DimmedModal 기본 틀 잡기

---
+++ src/components/Modal/DimmedModal/DimmedModal.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DimmedBgContainer = styled.div`
+  width: 600px;
+  @media (min-width: 360px) and (max-width: 600px) {
+    width: 360px;
+  }
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  background-color: rgba(39, 39, 39, 0.48);
+`
+
+export const DimmedMdContainer = styled.div`
+  width: 19rem;
+  height: 17rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
+`
