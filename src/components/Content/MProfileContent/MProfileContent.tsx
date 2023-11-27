16b6bc0f0d83c8333ae47fd2f84817cdfbec6dd1
leehyewon0531 <ifpffk@naver.com>
2023-11-27 23:26:23 +0900

    RAC-159 style: MProfileContent 스타일 잡기

--- src/components/Content/MProfileContent/MProfileContent.tsx
+++ src/components/Content/MProfileContent/MProfileContent.tsx
@@ -6,9 +6,11 @@ import { MProfileContainer } from './MProfileContent.styled';
 function MProfileContent() {
   return(
     <MProfileContainer>
-      <div>프로필을 작성하지 않으면<br />멘토링을 진행할 수 없어요</div>
+      <div id='profile-guide-msg'>프로필을 작성하지 않으면<br />멘토링을 진행할 수 없어요</div>
       <Image id="x-icon" src={x_icon} alt="닫기 버튼" />
-      <NextBtn kind="route" url="/add-profile" btnText="프로필 등록하기" />
+      <div id='btn-styled-wrapper'>
+        <NextBtn kind="route" url="/add-profile" btnText="프로필 등록하기" />
+      </div>
     </MProfileContainer>
   )
 }
