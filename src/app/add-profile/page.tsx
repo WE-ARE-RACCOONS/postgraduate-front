ade6d3e0ccd511d5e9543e42fe3e076667196dd9
leehyewon0531 <ifpffk@naver.com>
2023-11-28 22:02:21 +0900

    RAC-166 refactor: ProgressBar 분리

--- src/app/add-profile/page.tsx
+++ src/app/add-profile/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+import ProgressBar from "@/components/Bar/ProgressBar";
+import styled from "styled-components";
+
 function AddProfilePage() {
-  return <div>프로필 등록 페이지</div>;
+
+  return (
+    <AddProfilePageContainer>
+      <ProgressBar activeTab={1} />
+    </AddProfilePageContainer>
+  );
 }
 
 export default AddProfilePage;
+
+const AddProfilePageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`
---
+++ src/components/Bar/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { StepperContainer, StepperTab } from "./ProgressBar.styled";
+
+function ProgressBar({ activeTab }: { activeTab: number }) {
+
+  useEffect(() => {
+    const activeStep = document.getElementById(`stepper-tab-${activeTab}`);
+    activeStep?.classList.add('active');
+  }, []);
+
+  return(
+    <StepperContainer>
+      <StepperTab>
+        <div id="stepper-tab-0" className="stepper-tab"></div>
+      </StepperTab>
+      <StepperTab>
+        <div id="stepper-tab-1" className="stepper-tab"></div>
+      </StepperTab>
+      <StepperTab>
+        <div id="stepper-tab-2" className="stepper-tab"></div>
+      </StepperTab>
+    </StepperContainer>
+  )
+}
+
+export default ProgressBar;
