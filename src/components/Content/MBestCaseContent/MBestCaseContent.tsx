55b1d27b83ca1530479b3111153d3db6c8814fdf
leehyewon0531 <ifpffk@naver.com>
2023-11-29 15:41:51 +0900

    RAC-166 feat: 우수 사례 모달 x 버튼 구현

--- src/components/Content/MBestCaseContent/MBestCaseContent.tsx
+++ src/components/Content/MBestCaseContent/MBestCaseContent.tsx
@@ -1,6 +1,19 @@
+import { MBestCaseContainer } from "./MBestCaseContent.styled";
+import Image from 'next/image';
+import x_icon from '../../../../public/x.png';
+
 function MBestCaseContent({ modalHandler } : { modalHandler: () => void }) {
   return(
-    <div>우수사례 컨텐츠</div>
+    <MBestCaseContainer>
+      <Image
+        id="x-icon"
+        src={x_icon}
+        alt="닫기 버튼"
+        sizes="(max-width: 600px) 3.rem"
+        priority
+        onClick={modalHandler}
+      />
+    </MBestCaseContainer>
   )
 }
 
