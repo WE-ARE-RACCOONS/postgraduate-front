d6c89269ed35c676c9c505a0baca7ffb150e4c40
leehyewon0531 <ifpffk@naver.com>
2023-12-04 18:02:27 +0900

    RAC-198 feat: SalaryBox 컴포넌트 기능 완성

--- src/components/Box/SalaryBox/SalaryBox.styled.ts
+++ src/components/Box/SalaryBox/SalaryBox.styled.ts
@@ -11,3 +11,18 @@ export const SalaryBlock = styled.div`
   width: 9.2rem;
   height: 4rem;
 `
+
+export const SalaryDesc = styled.div`
+  width: max-content;
+  height: 1.25rem;
+  font-size: 14px;
+  font-weight: 700;
+`
+
+export const SalaryValue = styled.div`
+  width: max-content;
+  height: 1.25rem;
+  font-size: 14px;
+  font-weight: 700;
+  color: #3bf;
+`
--- src/components/Box/SalaryBox/SalaryBox.tsx
+++ src/components/Box/SalaryBox/SalaryBox.tsx
@@ -1,11 +1,32 @@
 import { SalaryBoxProps } from "@/types/box/salaryBox";
-import { SalaryBlock, SalaryBoxContainer } from "./SalaryBox.styled";
+import { SalaryBlock, SalaryBoxContainer, SalaryDesc, SalaryValue } from "./SalaryBox.styled";
 
 function SalaryBox(props: SalaryBoxProps) {
+  
+  function addCommas(amount: number) {
+    return amount.toLocaleString('ko-KR');
+  }
+
+  function formatDate(dateStr: string) {
+    if(!dateStr) return '';
+    
+    const splitDate = dateStr.split('-');
+    const month = parseInt(splitDate[1]);
+    const date = splitDate[2];
+
+    return `${month}월 ${date}일`;
+  }
+
   return(
     <SalaryBoxContainer>
-      <SalaryBlock>{props.salaryAmount}</SalaryBlock>
-      <SalaryBlock>{props.salaryDate}</SalaryBlock>
+      <SalaryBlock>
+        <SalaryDesc>정산 예정액</SalaryDesc>
+        <SalaryValue>{addCommas(props.salaryAmount)}원</SalaryValue>
+      </SalaryBlock>
+      <SalaryBlock>
+        <SalaryDesc>다음 정산 예정일</SalaryDesc>
+        <SalaryValue>{formatDate(props.salaryDate)}</SalaryValue>
+      </SalaryBlock>
     </SalaryBoxContainer>
   )
 }
