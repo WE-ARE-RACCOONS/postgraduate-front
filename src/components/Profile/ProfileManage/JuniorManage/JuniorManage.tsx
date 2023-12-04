94a5493b248c50645f98f7d57d53b51fc4b98592
yeonssu21 <kimys9295@naver.com>
2023-12-04 21:46:21 +0900

    RAC-192 fix: 충돌 해결

--- src/components/Profile/ProfileManage/JuniorManage/JuniorManage.tsx
+++ src/components/Profile/ProfileManage/JuniorManage/JuniorManage.tsx
@@ -2,11 +2,39 @@ import { JuniorManageContainer } from './JuniorManage.styled';
 import ContentComponent from '../../Box/ContentBox';
 import TitleComponent from '../../Box/TitleBox';
 import { useRouter } from 'next/navigation';
-
-function JuniorManage() {
+import { NotSeniorProps } from '@/types/modal/mypage';
+import useAuth from '@/hooks/useAuth';
+import { useAtom } from 'jotai';
+import { socialIdAtom } from '@/stores/signup';
+import axios from 'axios';
+function JuniorManage(props: NotSeniorProps) {
   const router = useRouter();
   const handleProfileEditClick = () => {
     router.push('/mypage/edit');
+  };
+  const { getAccessToken } = useAuth();
+  const [socialId, setSocialId] = useAtom(socialIdAtom);
+  const handleClick = async () => {
+    try {
+      const Token = getAccessToken();
+      if (Token) {
+        const headers = {
+          Authorization: `Bearer ${Token}`,
+        };
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/user/me/role`,
+          { headers },
+        );
+        if (response.data.data === true) {
+          setSocialId(response.data.socialId);
+        }
+        if (response.data.data === false) {
+          props.modalHandler();
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching data from the server:', error);
+    }
   };
 
   return (
@@ -16,7 +44,7 @@ function JuniorManage() {
         content="내 정보 수정"
         onClick={handleProfileEditClick}
       />
-      <ContentComponent content="대학원선배 회원으로 변경" />
+      <ContentComponent content="대학원선배 회원으로 변경" onClick={handleClick} />
     </JuniorManageContainer>
   );
 }
