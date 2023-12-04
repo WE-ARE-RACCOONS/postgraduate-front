4ca7bdfe05173bc241e67e8144ece20bd1457a01
leehyewon0531 <ifpffk@naver.com>
2023-12-04 16:43:29 +0900

    RAC-198 fix: login.tsx 오류 해결

--- src/components/kakao/login.tsx
+++ src/components/kakao/login.tsx
@@ -1,26 +1,26 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuth from '@/hooks/useAuth';
 const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
 const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
 
 function Login() {
   const { getAccessToken } = useAuth();
-  const Token = getAccessToken();
+  const [Token, setToken] = useState<string | null | undefined>('');
   const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
     window.location.href = link;
   };
 
+  useEffect(() => {
+    setToken(getAccessToken());
+  }, []);
+
   return (
     <div>
-      {Token ? (
-        ''
-      ) : (
-        <button type="button" onClick={loginHandler}>
-          로그인 하기
-        </button>
+      {Token ? (<></>) : (
+        <button type="button" onClick={loginHandler}>로그인 하기</button>
       )}
     </div>
   );
