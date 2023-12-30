62bce3ca33001fe2c91a99eac33c810b53952d8c
yeonssu21 <kimys9295@naver.com>
2023-12-30 17:37:05 +0900

    RAC-239 feat: 취소 모달 디자인

--- src/components/Mentoring/MentoringCancel/MentoringCancel.styled.ts
+++ src/components/Mentoring/MentoringCancel/MentoringCancel.styled.ts
@@ -1,22 +1,52 @@
 import styled from 'styled-components';
 
 export const MentoringCancelBox = styled.div`
-  position: fixed;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 19rem;
-height: 17.6875rem;
-  z-index: 2;
-  background-color: white;
-
-  @media (min-width: 360px) and (max-width: 600px) {
-    width: 360px;
-  }
+ margin-bottom: 2rem;
 `;
-export const CancelBtn = styled.button``;
-export const NoCancelBtn = styled.button`
+export const MCSub = styled.div`
+color: #868E96;
+text-align: center;
+font-family: Noto Sans JP;
+font-size: 1rem;
+font-style: normal;
+font-weight: 400;
+line-height: 140%; /* 1.4rem */
+width: 15rem;
+margin-left: 1.85rem;
+margin-top: 1.5rem;
+`;
+export const MCMain = styled.div`
+color: #333;
+text-align: center;
+font-family: Noto Sans JP;
+font-size: 1.25rem;
+font-style: normal;
+font-weight: 700;
+line-height: 130%; /* 1.625rem */
+`;
+export const CancelBtn = styled.button`
 display: flex;
-width: 18.4375rem;
+padding: 1rem 3em;
+justify-content: center;
+align-items: center;
+border-radius: 0.75rem;
+background: #FF5757;
+color: #FFF;
+text-align: center;
+font-family: Pretendard;
+font-size: 1.125rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+border: none;
+margin-left:0.5rem;
+margin-right: 0.44rem;
+`;
+export const OkayBtn = styled.button`
+display: flex;
+width: 17rem;
+margin-top:1.81rem;
+margin-left: 1rem;
 padding: 1rem 5.8125rem;
 justify-content: center;
 align-items: center;
@@ -31,4 +61,20 @@ font-size: 1.125rem;
 font-style: normal;
 font-weight: 700;
 line-height: normal;
+`;
+export const NoCancelBtn = styled.button`
+display: flex;
+padding: 1rem 3em;
+justify-content: center;
+align-items: center;
+border-radius: 0.75rem;
+background: #DEE2E6;
+color: #212529;
+text-align: center;
+font-family: Pretendard;
+font-size: 1.125rem;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+border: none;
 `;
--- src/components/Mentoring/MentoringCancel/MentoringCancel.tsx
+++ src/components/Mentoring/MentoringCancel/MentoringCancel.tsx
@@ -7,9 +7,14 @@ import {
   MentoringCancelBox,
   CancelBtn,
   NoCancelBtn,
+  OkayBtn,
+  MCMain,
+  MCSub
 } from './MentoringCancel.styled';
 import { useEffect, useState } from 'react';
-
+import Image from 'next/image';
+import state from '@/../../public/state.png'
+import cState from '@/../../public/cState.png'
 function MentoringCancel(props: ModalMentoringclProps) {
   const [data, setData] = useState<MentoringData[] | null>(null);
   const { getAccessToken } = useAuth();
@@ -21,11 +26,6 @@ function MentoringCancel(props: ModalMentoringclProps) {
     if (props.onClick) props.onClick();
   };
 
-  useEffect(() => {
-    if (props.mentoringId !== 0) {
-      cancelMentoring();
-    }
-  }, []);
 
   const cancelMentoring = async () => {
     try {
@@ -34,39 +34,68 @@ function MentoringCancel(props: ModalMentoringclProps) {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${Token}`,
       };
-
-      const response = await fetch(
+  
+      const response = await axios.patch(
         `${process.env.NEXT_PUBLIC_SERVER_URL}/mentoring/me/${props.mentoringId}/cancel`,
         {
-          method: 'PATCH',
-          headers,
-          body: JSON.stringify({
-            mentoringId: props.mentoringId,
-          }),
+          mentoringId: props.mentoringId,
         },
+        { headers }
       );
-      const responseData = await response.json();
-      setData(responseData);
-      setShowCancelButton(false);
-      setCancelStatus(responseData.success ? '취소되었습니다' : '취소 실패');
-      // 서버측 성공 코드 확인후 수정예정
+      setData(response.data);
+      console.log(response.data)
+      setShowCancelButton(true);
+      if (response.data.code === "MT201") {
+        setCancelStatus('취소되었습니다');
+      } else {
+        setCancelStatus('취소 실패');
+      }
     } catch (error) {
       console.error('Error cancelling mentoring:', error);
     }
   };
   return (
+<>
+    {cancelStatus ? <Image
+            id="cState"
+            src={cState}
+            alt="확인상태"
+            width={65}
+            height={65}
+            style={{margin:'0.5rem 7.5rem '}}
+            priority/>: 
+    <Image
+            id="state"
+            src={state}
+            alt="경고상태"
+            width={65}
+            height={65}
+            style={{margin:' 0.5rem 7.5rem'}}
+            priority/>}
     <MentoringCancelBox>
-      {cancelStatus || '멘토링 신청을 취소하시겠어요?'}
-      {showCancelButton ? (
-        <div>
-          <CancelBtn onClick={cancelMentoring}>취소</CancelBtn>
-          <NoCancelBtn onClick={() => handleClick()}>아니요</NoCancelBtn>
-        </div>
+      <MCMain>
+      {cancelStatus ? `${cancelStatus}` : '멘토링 신청을 취소하시겠어요?'}
+      </MCMain>
+      {cancelStatus ? (
+        <>
+        <MCSub>환불은 카드사 정책에 따라
+영업일 기준 2~3일이 소요됩니다.</MCSub>
+        <OkayBtn onClick={() => handleClick()}>확인했어요</OkayBtn>
+        </>
       ) : (
-        <NoCancelBtn onClick={() => handleClick()}>확인했어요</NoCancelBtn>
+        <>
+        <MCSub>반복되는 신청 취소시 멘토링 매칭에 
+불이익이 있을 수 있습니다.</MCSub>
+        <div style={{display:'flex',marginTop:'1.8rem'}}>
+          <CancelBtn onClick={cancelMentoring}>취소</CancelBtn>
+          <NoCancelBtn onClick={() => handleClick()}>닫기</NoCancelBtn>
+        </div>
+        </>
       )}
     </MentoringCancelBox>
+    </>
   );
+ 
 }
 
 export default MentoringCancel;
