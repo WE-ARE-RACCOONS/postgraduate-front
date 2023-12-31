e907ad72f235b92520bd8843841c8dadc0745326
leehyewon0531 <ifpffk@naver.com>
2023-12-31 20:02:13 +0900

    RAC-245 style: ProgressBar 스타일 수정

--- src/app/mentoring-apply/[seniorId]/question/page.tsx
+++ src/app/mentoring-apply/[seniorId]/question/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+import ProgressBar from "@/components/Bar/ProgressBar";
+import BackHeader from "@/components/Header/BackHeader";
+import styled from "styled-components";
+
 function MentoringApplyQuestionPage() {
   return(
-    <div>멘토링 신청 페이지</div>
+    <MAQContainer>
+      <BackHeader headerText="신청서 작성" />
+      <ProgressBar activeNum={0} />
+    </MAQContainer>
   )
 }
+
+const MAQContainer = styled.div`
+  width: inherit;
+  height: auto;
+  position: relative;
+`
 
 export default MentoringApplyQuestionPage;
