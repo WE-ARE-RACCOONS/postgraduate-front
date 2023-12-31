abd1f76dfe732c96947b4c9cc756085d90f955ea
leehyewon0531 <ifpffk@naver.com>
2023-12-31 20:23:22 +0900

    RAC-245 feat: 유의사항 컴포넌트 완성

--- src/app/mentoring-apply/[seniorId]/question/page.tsx
+++ src/app/mentoring-apply/[seniorId]/question/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import ProgressBar from "@/components/Bar/ProgressBar";
 import BackHeader from "@/components/Header/BackHeader";
+import { MENTORING_NOTICE } from "@/constants/form/cMentoringApply";
 import styled from "styled-components";
 
 function MentoringApplyQuestionPage() {
@@ -8,6 +9,13 @@ function MentoringApplyQuestionPage() {
     <MAQContainer>
       <BackHeader headerText="신청서 작성" />
       <ProgressBar activeNum={0} />
+      <MQANoticeBox>
+        <div id="mentoring-notice-text-box">
+          <div id="mentoring-notice-text" className="mentoring-notice">{MENTORING_NOTICE.noticeText}</div>
+          <div id="mentoring-send-text" className="mentoring-notice">{MENTORING_NOTICE.sendText}</div>
+          <div id="mentoring-alert-text" className="mentoring-notice">{MENTORING_NOTICE.alertText}</div>
+        </div>
+      </MQANoticeBox>
     </MAQContainer>
   )
 }
@@ -16,6 +24,40 @@ const MAQContainer = styled.div`
   width: inherit;
   height: auto;
   position: relative;
+`
+
+const MQANoticeBox = styled.div`
+  width: 95%;
+  height: 5.75rem;
+  border-radius: 8px;
+  background-color: #F8F9FA;
+  position: absolute;
+  top: 4.75rem;
+  left: 50%;
+  transform: translateX(-50%);
+
+  #mentoring-notice-text-box {
+    width: max-content;
+    height: 4rem;
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+  }
+
+  .mentoring-notice {
+    font-size: 14px;
+    line-height: 140%;
+    letter-spacing: -0.5px;
+  }
+
+  #mentoring-notice-text {
+    font-weight: 700;
+    margin-bottom: 0.25rem;
+  }
+
+  #mentoring-alert-text {
+    color: #2FC4B2;
+  }
 `
 
 export default MentoringApplyQuestionPage;
---
+++ src/constants/form/cMentoringApply.ts
@@ -0,0 +1,5 @@
+export const MENTORING_NOTICE = {
+  noticeText: '유의사항',
+  sendText: '신청서는 작성 즉시 대학원 선배에게 전송됩니다!',
+  alertText: '이후 수정할 수 없으니 신중하게 작성해주세요.'
+}
