116d562ffd619cd15382c683eb846626197fb402
leehyewon0531 <ifpffk@naver.com>
2024-01-02 17:57:31 +0900

    RAC-253 feat: 프로필 등록하기 버튼 라우팅 연결

--- src/components/Content/SNotRegistered/SNotRegistered.tsx
+++ src/components/Content/SNotRegistered/SNotRegistered.tsx
@@ -2,14 +2,17 @@ import { PNRBtn, PNRContainer, PNRDesc, PNRTitle } from "./SNotRegistered.styled
 import Image from "next/image";
 import x_icon from '../../../../public/x.png';
 import { PROFILE_NOT_REGISTERED } from "@/constants/modal/dimmedModal";
+import { useRouter } from "next/navigation";
 
 function SNotRegistered({ modalHandler } : { modalHandler: () => void }) {
+  const router = useRouter();
+
   return(
     <PNRContainer>
       <Image id="x-icon" src={x_icon} alt="X 버튼" onClick={modalHandler} />
       <PNRTitle>{PROFILE_NOT_REGISTERED.title}</PNRTitle>
       <PNRDesc>{PROFILE_NOT_REGISTERED.desc}</PNRDesc>
-      <PNRBtn>{PROFILE_NOT_REGISTERED.btnText}</PNRBtn>
+      <PNRBtn onClick={() => {router.push('/add-profile')}}>{PROFILE_NOT_REGISTERED.btnText}</PNRBtn>
     </PNRContainer>
   )
 }
