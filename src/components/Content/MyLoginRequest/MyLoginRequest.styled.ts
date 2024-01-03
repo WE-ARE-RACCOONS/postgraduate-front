1e28fed843938bbf43df3b3978411388e4fc7cb5
yeonssu21 <kimys9295@naver.com>
2024-01-03 19:07:39 +0900

    RAC-271 feat: 마이페이지 비로그인 모달 디자인

--- src/components/Content/MyLoginRequest/MyLoginRequest.styled.ts
+++ src/components/Content/MyLoginRequest/MyLoginRequest.styled.ts
@@ -15,11 +15,72 @@ export const MyLoginRequestBox = styled.div`
 `;
 export const MLBoxTop = styled.div`
   display: flex;
+  padding: 1rem;
 `;
 export const MLBoxMiddle = styled.div`
-  margin-top: 5rem;
+align-items: center;
+display: flex;
+justify-content: center;
 `;
 export const Logo = styled.div`
-  border: 1px solid blue;
+  display: flex;
+  .none-name {
+    font-size: 1.3rem;
+  }
+  .bold-name {
+    font-size: 1.3rem;
+    font-weight: 700;
+  }
+  
 `;
-export const MLBoxBottom = styled.div``;
+export const MLBoxBottom = styled.div`
+margin-left: 1.5rem;
+#mylogin-req-msg{
+  color: #212529;
+font-family: Pretendard;
+font-size: 1.25rem;
+font-style: normal;
+font-weight: 400;
+line-height: 120%; /* 1.5rem */
+letter-spacing: -0.03125rem;
+}
+#mylogin-req-msg-bold{
+  color: #212529;
+font-family: Pretendard;
+font-size: 1.5rem;
+font-style: normal;
+font-weight: 700;
+line-height: 120%;
+letter-spacing: -0.03125rem;
+}
+#mylogin-req-msg-large{
+  margin-bottom: 1rem;
+  display: flex;
+  color: #212529;
+font-family: Pretendard;
+font-size: 1.5rem;
+font-style: normal;
+font-weight: 400;
+line-height: 120%;
+letter-spacing: -0.03125rem;
+}
+#mylogin-req-msg-color{
+  color: #2FC4B2;
+font-family: Pretendard;
+font-size: 1rem;
+font-style: normal;
+font-weight: 400;
+line-height: 140%; /* 1.4rem */
+letter-spacing: -0.03125rem;
+}
+#mylogin-req-msg-small{
+  display: flex;
+  color: #868E96;
+font-family: Pretendard;
+font-size: 1rem;
+font-style: normal;
+font-weight: 400;
+line-height: 140%;
+letter-spacing: -0.03125rem;
+}
+`;
