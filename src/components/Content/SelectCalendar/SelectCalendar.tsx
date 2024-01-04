b32df5008952fcdc76a3dbcdbc097f6998e7452e
leehyewon0531 <ifpffk@naver.com>
2024-01-04 16:30:53 +0900

    RAC-275 feat: 일정 선택 모달 헤더 구현

--- src/components/Content/SelectCalendar/SelectCalendar.tsx
+++ src/components/Content/SelectCalendar/SelectCalendar.tsx
@@ -1,6 +1,21 @@
+import BackHeader from "@/components/Header/BackHeader";
+import { SelectCalendarContainer, SelectCalendarHeader } from "./SelectCalendar.styled";
+import Image from "next/image";
+import back_arrow from '../../../../public/arrow.png';
+
 function SelectCalendar({ modalHandler } : { modalHandler: () => void }) {
   return(
-    <div>SelectCalendar</div>
+    <SelectCalendarContainer>
+      <SelectCalendarHeader>
+        <Image
+          id="back-arrow-img"
+          src={back_arrow}
+          alt="뒤로가기 화살표"
+          onClick={modalHandler}
+        />
+        <div id="header-text">일정 선택</div>
+      </SelectCalendarHeader>
+    </SelectCalendarContainer>
   )
 }
 
