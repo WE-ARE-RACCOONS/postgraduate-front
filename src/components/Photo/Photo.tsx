bbb434e62db0a773261d51d3f84c44954ebf1eba
yeonssu21 <kimys9295@naver.com>
2024-01-04 17:04:12 +0900

    RAC-272 feat:대학원생 사진 인증 페이지 디자인

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image';
 import camera from '@../../../public/check.png';
-function Photo({
-  handler,
-}: {
+import {AuthImgBtn} from './Photo.styled'
+import upload from '@../../../public/upload.png';
+type PhotoProps = {
   handler: React.Dispatch<React.SetStateAction<File | null>>;
-}) {
+  type: 'camera' | 'auth'; // 타입 추가
+};
+function Photo({ handler, type }: PhotoProps){
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     handler(e.currentTarget.files ? e.currentTarget.files[0] : null);
   };
 
   return (
     <label htmlFor="photo-upload" style={{ cursor: 'pointer' }}>
-      <Image
+      {type =='camera'&&<Image
         src={camera}
         alt="camera"
         style={{ width: '32px', height: '32px', objectFit: 'cover' }}
+      />}
+      {type =='auth'&&<AuthImgBtn>
+      <Image
+        src={upload}
+        alt="upload"
+        style={{ width: '17px', height: '17px', objectFit: 'cover' }}
       />
+        
+        사진선택</AuthImgBtn>}
       <input
         type="file"
         accept="image/*"
