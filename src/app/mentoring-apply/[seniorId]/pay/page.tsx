8b6dc196eb34472d6a0d09476e31b24c96f60b33
leehyewon0531 <ifpffk@naver.com>
2024-01-12 22:23:48 +0900

    RAC-266 feat: 선택일정 부분 완성

--- src/app/mentoring-apply/[seniorId]/pay/page.tsx
+++ src/app/mentoring-apply/[seniorId]/pay/page.tsx
@@ -5,8 +5,33 @@ import RoundedImage from "@/components/Image/RoundedImage";
 import styled from "styled-components";
 import user_icon from '../../../../../public/user.png';
 import AuthLabeledText from "@/components/Text/AuthLabeledText";
+import { useAtomValue } from "jotai";
+import { firAbleTimeAtom, secAbleTimeAtom, thiAbleTimeAtom } from "@/stores/mentoring";
+import { useEffect } from "react";
 
 function MentoringApplyPayPage() {
+  const firstTime = useAtomValue(firAbleTimeAtom);
+  const secondTime = useAtomValue(secAbleTimeAtom);
+  const thirdTime = useAtomValue(thiAbleTimeAtom);
+
+  const formatTime = (time: string) => {
+    if(!time) return '';
+
+    let result = '';
+    const timeArr = time.split('-');
+    if(timeArr.length >= 5) {
+      const month = Number(timeArr[1]);
+      const date = Number(timeArr[2]);
+      const hour = timeArr[3];
+      const min = timeArr[4];
+
+      result += `${month}월 `;
+      result += `${date}일 `;
+      result += (Number(min) == 0) ? `${hour}시 00분 ~ ${hour}시 30분` : `${hour}시 30분 ~ ${Number(hour) + 1}시 00분`;
+      return result;
+    } else return '';
+  }
+
   return (
     <MAPContainer>
       <BackHeader headerText="멘토링 결제 정보" />
@@ -25,6 +50,27 @@ function MentoringApplyPayPage() {
               <div id="map-info-lab">Computer Systems and Intelligence</div>
             </div>
           </MAPInfoWrapper>
+        </MAPBox>
+        <MAPBox>
+          <MAPTimeWrapper>
+            <MAPTitle>선택일정</MAPTitle>
+            <div id="map-time-list">
+              <ol>
+                <li>
+                  <div className="map-time-text">첫번째 일정 :</div>
+                  <div className="map-time-value">{formatTime(firstTime)}</div>
+                </li>
+                <li>
+                  <div className="map-time-text">두번째 일정 :</div>
+                  <div className="map-time-value">{formatTime(secondTime)}</div>
+                </li>
+                <li>
+                  <div className="map-time-text">세번째 일정 :</div>
+                  <div className="map-time-value">{formatTime(thirdTime)}</div>
+                </li>
+              </ol>
+            </div>
+          </MAPTimeWrapper>
         </MAPBox>
       </MAPContent>
     </MAPContainer>
@@ -61,6 +107,7 @@ const MAPBox = styled.div`
   border-radius: 8px;
   background-color: #FFF;
   padding: 1rem;
+  margin-bottom: 0.75rem;
   border: 1px solid #000;
 `
 
@@ -88,6 +135,35 @@ const MAPInfoWrapper = styled.div`
       font-size: 12px;
       color: #212529;
     }
+  }
+`
+
+const MAPTimeWrapper = styled.div`
+  width: 100%;
+
+  ol {
+    list-style: none;
+    font-size: 14px;
+    width: 90%;
+    height: 4.69rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 0.32rem;
+  }
+
+  li {
+    display: flex;
+    white-space: nowrap;
+  }
+
+  .map-time-text {
+    color: #868E96;
+    margin-right: 2.375rem;
+  }
+
+  .map-time-value {
+    
   }
 `
 
