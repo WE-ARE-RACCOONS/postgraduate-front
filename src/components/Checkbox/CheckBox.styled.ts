432e5b901d2e303c603bf1e2a16d1e7cdbc2545c
연수 <75175148+yeonsuu21@users.noreply.github.com>
2024-01-20 15:37:35 +0900

    Create CheckBox.styled.ts

---
+++ src/components/Checkbox/CheckBox.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledInput = styled.input`
+  appearance: none;
+  border: 1.5px solid gainsboro;
+  border-radius: 0.35rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 0.56rem;
+
+  &:checked {
+  }
+`;
