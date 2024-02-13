375a31b1b0adfa7d81614c6608a478c8d31f2aac
yeonssu21 <kimys9295@naver.com>
2024-02-13 20:22:41 +0900

    RAC-297 feat: 파일 정리

--- src/app/mentoring-apply/done/page.tsx
+++ src/app/mentoring-apply/done/page.tsx
@@ -4,9 +4,63 @@ import Image from 'next/image';
 import cState from '../../../../public/cState.png';
 import { MENTORING_DONE_TEXT } from '@/constants/mentoring/done';
 import { useRouter } from 'next/navigation';
-
+import axios from 'axios';
+import { firAbleTimeAtom, orderIdAtom, paySeniorIdAtom, questionAtom, secAbleTimeAtom, subjectAtom, thiAbleTimeAtom } from '@/stores/mentoring';
+import { useAtomValue } from 'jotai';
+import React, { useEffect } from 'react'
+import useAuth from '@/hooks/useAuth';
 function MentoringApplyDonePage() {
   const router = useRouter();
+  const oderId = useAtomValue(orderIdAtom);
+  const topic = useAtomValue(subjectAtom);
+  const question = useAtomValue(questionAtom);
+  const firstTime = useAtomValue(firAbleTimeAtom);
+  const secondTime = useAtomValue(secAbleTimeAtom);
+  const thirdTime = useAtomValue(thiAbleTimeAtom);
+  const paySeniorId = useAtomValue(paySeniorIdAtom);
+  const { getAccessToken } = useAuth();
+  const payHandler = () => {
+    const accessTkn = getAccessToken();
+    if (
+      accessTkn &&
+      topic &&
+      question &&
+      firstTime &&
+      secondTime &&
+      thirdTime
+    ) {
+      const timeArr = [firstTime, secondTime, thirdTime];
+
+      axios
+        .post(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/mentoring/applying`,
+          {
+            seniorId: paySeniorId,
+            orderId:oderId,
+            topic: topic,
+            question: question,
+            date: timeArr.join(','),
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${accessTkn}`,
+            },
+          },
+        )
+        .then((response) => {
+          const res = response.data;
+          if (res.code == 'MT202') {
+            //  router.push('/mentoring-apply/done');
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+  useEffect(() => {
+    payHandler(); // This will be called after the component mounts
+  }, []);
 
   return (
     <MADContainer>
