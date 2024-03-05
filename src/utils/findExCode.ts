16985e5fa19fb845a0294b441307006a3d9cc5aa
leehyewon0531 <ifpffk@naver.com>
2024-03-05 15:14:24 +0900

    RAC-320 chore: Prettier format fix 실행

--- src/utils/findExCode.ts
+++ src/utils/findExCode.ts
@@ -1,6 +1,6 @@
-export default function findExCode (code: string) {
-  const targetCode = ["EX200", "EX201", "EX300", "EX903"];
+export default function findExCode(code: string) {
+  const targetCode = ['EX200', 'EX201', 'EX300', 'EX903'];
 
-  if(targetCode.includes(code)) return true;
+  if (targetCode.includes(code)) return true;
   else return false;
 }
