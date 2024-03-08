db0e36f0b82645ae5c4b5f439363989a5adea9bd
leehyewon0531 <ifpffk@naver.com>
2024-03-08 11:50:17 +0900

    RAC-320 chore: Prettier format fix 실행

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
+import Image from 'next/image';
 import footer from '../../../public/footer.png';
 
 function Footer() {
-  return(
-    <Image src={footer} alt="푸터 이미지" width={360} height={188} />
-  )
+  return <Image src={footer} alt="푸터 이미지" width={360} height={188} />;
 }
 
 export default Footer;
