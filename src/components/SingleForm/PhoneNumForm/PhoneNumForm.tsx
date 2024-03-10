36c84fa5a44aa3a29e3436a919f3102e3b952d06
leehyewon0531 <ifpffk@naver.com>
2024-03-10 16:07:22 +0900

    RAC-320 fix: 회원가입 뒤로가기 시 입력했던 휴대폰 번호 값 유지

--- src/components/SingleForm/PhoneNumForm/PhoneNumForm.tsx
+++ src/components/SingleForm/PhoneNumForm/PhoneNumForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 import SingleValidator from '@/components/Validator/SingleValidator';
 import { PhoneNumContainer, NumFont } from './PhoneNumForm.styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSetAtom, useAtom } from 'jotai';
 import { phoneNum, phoneNumValidation } from '@/stores/signup';
 
@@ -10,6 +10,13 @@ function PhoneNumForm({ defaultValue }: { defaultValue?: string }) {
   const [fullNum, setFullNum] = useAtom(phoneNum);
   const [availability, useAvailability] = useState(false);
   const setValidation = useSetAtom(phoneNumValidation);
+
+  useEffect(() => {
+    if(fullNum) {
+      const inputForm = document.getElementById('phone-num-input') as HTMLInputElement;
+      if(inputForm) inputForm.value = fullNum;
+    }
+  }, []);
 
   function checkPhoneNum(e: React.ChangeEvent<HTMLInputElement>) {
     if (!flag) setFlag(true);
