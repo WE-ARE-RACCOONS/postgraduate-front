c753e5fadf775c03a727d4f96cb22d4954872538
yeonssu21 <kimys9295@naver.com>
2024-03-19 14:26:21 +0900

    RAC-324 chore: format:fix

--- src/components/Content/MBestCaseContent/MBestCaseContent.tsx
+++ src/components/Content/MBestCaseContent/MBestCaseContent.tsx
@@ -1,6 +1,6 @@
 import { MBestCaseContainer } from './MBestCaseContent.styled';
 import Image from 'next/image';
-import bestCase from '@/../../public/best_sample.png'
+import bestCase from '@/../../public/best_sample.png';
 import x_icon from '../../../../public/x.png';
 import user_img from '../../../../public/user.png';
 import RoundedImage from '@/components/Image/RoundedImage';
@@ -12,11 +12,7 @@ import TextField from '@/components/Text/TextField';
 function MBestCaseContent({ modalHandler }: { modalHandler: () => void }) {
   return (
     <MBestCaseContainer>
-      <Image
-      src={bestCase}
-      alt='bestCase'
-      width={360}
-      height={885}/>
+      <Image src={bestCase} alt="bestCase" width={360} height={885} />
     </MBestCaseContainer>
   );
 }
