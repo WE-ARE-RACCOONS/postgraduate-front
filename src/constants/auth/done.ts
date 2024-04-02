dc97e496a77dab7736ba92c758b468a0efc99f4e
leehyewon0531 <ifpffk@naver.com>
2024-04-02 17:11:05 +0900

    RAC-329 feat & fix : 마페에서 선배 인증시 플로우 수정 & 선배 인증 완료 페이지 제작

---
+++ src/constants/auth/done.ts
@@ -0,0 +1,9 @@
+export const AUTH_DONE_MSG = {
+  topText: '인증하기',
+  title: '대학원 인증이 신청되었어요!',
+  description: `영업일 기준 48시간 안에 선배 회원으로 승인돼요\n승인되면 카카오 알림톡으로 알려드릴게요!`,
+  firGuide: `\'내 프로필 수정\'에서 프로필을 완성하면\n`,
+  secGuide: '멘토링을 시작',
+  thiGuide: '할 수 있어요.',
+  btnText: '확인했어요'
+}
