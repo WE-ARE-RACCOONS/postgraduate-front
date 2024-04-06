08e6fa1285450117df27e3527db8f0bf6505a7ab
yeonssu21 <kimys9295@naver.com>
2024-04-06 14:14:33 +0900

    RAC-334 fix: 희망 대학원 , 연구분야 작성 안해도 넘어가게 함

--- src/components/Button/SignUpBtn/SignUpBtn.styled.ts
+++ src/components/Button/SignUpBtn/SignUpBtn.styled.ts
@@ -19,3 +19,22 @@ export const SignUpBtnContainer = styled.button`
   line-height: normal;
   cursor: pointer;
 `;
+export const SignUpBtnNonContainer = styled.button`
+  display: flex;
+  width: 96%;
+  padding: 1rem 0rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.75rem;
+  background: #dee2e6;
+  border: none;
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  cursor: pointer;
+`;
