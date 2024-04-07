6b64d9a1ca80eaf40981a16d3fce796c4320589f
leehyewon0531 <ifpffk@naver.com>
2024-04-07 18:05:47 +0900

    RAC-333 fix: 연구주제 넘침 문제 수정

--- src/components/Card/KeywordCard/KeywordCard.styled.ts
+++ src/components/Card/KeywordCard/KeywordCard.styled.ts
@@ -2,32 +2,29 @@ import styled from 'styled-components';
 
 export const KeywordCardContainer = styled.div`
   width: 100%;
-  height: max-content;
-  min-height: 7.25rem;
+  height: auto;
   border-radius: 16px;
   background-color: #fff;
   box-shadow: 0px 0px 8px 0px rgba(73, 85, 101, 0.2);
   position: relative;
-  padding-bottom: 8rem;
+  padding: 1.2rem 1rem;
 
   #keyword-card-lab-name {
     width: max-content;
     height: 1.25rem;
     font-size: 14px;
     font-weight: 700;
-    position: absolute;
-    top: 1.2rem;
-    left: 1rem;
+    margin-bottom: 0.55rem;
   }
 `;
 
 export const KeywordCardArrayBox = styled.div`
   width: 90%;
-  height: max-content;
+  height: auto;
+  min-height: 4.125rem;
   display: flex;
   flex-wrap: wrap;
   gap: 0.625rem;
-  position: absolute;
   top: 3rem;
   left: 1rem;
 `;
