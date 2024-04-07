9697ee42ba9ed5df2c4b3c6f97b9e90880c4c242
leehyewon0531 <ifpffk@naver.com>
2024-04-07 18:14:22 +0900

    RAC-333 fix: 내멘토링 잘림 문제 수정

--- src/components/Bar/TapBar/SeniorTab/STabBrar.styled.ts
+++ src/components/Bar/TapBar/SeniorTab/STabBrar.styled.ts
@@ -57,5 +57,5 @@ export const TabResultContainer = styled.div`
 export const TabResult = styled.div`
   border-top: 1px solid #c2cede;
   height: 100%;
-  padding-bottom: 5.5rem;
+  padding-bottom: 7rem;
 `;
