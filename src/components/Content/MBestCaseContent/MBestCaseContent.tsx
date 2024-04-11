6bece8757859630a3fc32b4e1c16da7f4780cd9d
yeonssu21 <kimys9295@naver.com>
2024-04-11 18:13:37 +0900

    RAC-334 feat: 프로필 예시 뒤로가기 생성

--- src/components/Content/MBestCaseContent/MBestCaseContent.tsx
+++ src/components/Content/MBestCaseContent/MBestCaseContent.tsx
@@ -8,10 +8,12 @@ import DividedText from '@/components/Text/DividedText';
 import AuthLabeledText from '@/components/Text/AuthLabeledText';
 import BorderedText from '@/components/Text/BorderedText';
 import TextField from '@/components/Text/TextField';
+import BackHeader from '@/components/Header/BackHeader';
 
 function MBestCaseContent({ modalHandler }: { modalHandler: () => void }) {
   return (
     <MBestCaseContainer>
+      <BackHeader headerText="프로필 예시"/>
       <Image src={bestCase} alt="bestCase" width={360} height={885} />
     </MBestCaseContainer>
   );
