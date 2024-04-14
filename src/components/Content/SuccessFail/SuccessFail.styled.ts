502a5728e3e8de4beb72654e92d6fc13b51382e0
yeonssu21 <kimys9295@naver.com>
2024-04-14 13:33:33 +0900

    RAC-334 fix: 거절실패,성공 모달 ui 수정

--- src/components/Content/SuccessFail/SuccessFail.styled.ts
+++ src/components/Content/SuccessFail/SuccessFail.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 export const OkayBtn = styled.button`
   display: flex;
   width: 17rem;
-  margin-top: 3rem;
+  margin-top: 1.5rem;
   margin-left: 1.8rem;
   padding: 1rem 5.8125rem;
   justify-content: center;
@@ -20,7 +20,7 @@ export const OkayBtn = styled.button`
   font-weight: 700;
   line-height: normal;
 `;
-export const SuccessMid = styled.div`
+export const SuccessTop = styled.div`
   text-align: center;
   color: #333;
   text-align: center;
@@ -28,6 +28,17 @@ export const SuccessMid = styled.div`
   font-size: 1.25rem;
   font-style: normal;
   font-weight: 700;
-  line-height: 130%;
+  line-height: 130%; /* 1.625rem */
+`;
+export const MCSub = styled.div`
+  text-align: center;
+  margin-left: 13.5%;
+  color: #868e96;
+  font-family: Noto Sans JP;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; /* 1.4rem */
+  width: 15rem;
   margin-top: 1.5rem;
 `;
--- src/components/Content/SuccessFail/SuccessFail.tsx
+++ src/components/Content/SuccessFail/SuccessFail.tsx
@@ -3,8 +3,8 @@ import { useAtomValue } from 'jotai';
 import React from 'react';
 import Image from 'next/image';
 import acceptImage from '../../../../public/cState.png';
-import acceptNonImage from '../../../../public/cState_n.png';
-import { OkayBtn, SuccessMid } from './SuccessFail.styled';
+import acceptNonImage from '../../../../public/state.png';
+import { OkayBtn, SuccessTop ,MCSub} from './SuccessFail.styled';
 
 function SuccessFail({ modalHandler }: { modalHandler: () => void }) {
   const success = useAtomValue(SMCancelSuccessAtom);
@@ -24,8 +24,11 @@ function SuccessFail({ modalHandler }: { modalHandler: () => void }) {
               alt="멘토링 승인 축하 이미지"
             />
           </div>
-          <SuccessMid>멘토링 거절 성공</SuccessMid>
+          <div style={{}}>
+          <SuccessTop>거절 성공</SuccessTop>
+          <MCSub>멘토링 거절에 성공했습니다.</MCSub>
           <OkayBtn onClick={handleModalClose}>확인했어요</OkayBtn>
+          </div>
         </>
       ) : (
         <>
@@ -37,7 +40,8 @@ function SuccessFail({ modalHandler }: { modalHandler: () => void }) {
               alt="멘토링 승인 축하 이미지"
             />
           </div>
-          <SuccessMid>멘토링 거절 실패</SuccessMid>
+          <SuccessTop>거절 실패</SuccessTop>
+          <MCSub>멘토링 거절에 실패했습니다.</MCSub>
           <OkayBtn onClick={handleModalClose}>확인했어요</OkayBtn>
         </>
       )}
