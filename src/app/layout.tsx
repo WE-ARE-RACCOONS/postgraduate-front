831cb1a77083f8ac84c61eadbe45c28f1f3cfd1e
leehyewon0531 <ifpffk@naver.com>
2024-04-16 17:12:44 +0900

    RAC-338 chore: GTM 주석 해제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        {/* <GTMAnalytics /> */}
+        <GTMAnalytics />
         <GoogleAnalytics />
         <Providers>
           <StyledComponentsRegistry>
