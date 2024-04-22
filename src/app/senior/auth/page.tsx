ce34560183c71717d63bd6393b9eea150a5b1c50
leehyewon0531 <ifpffk@naver.com>
2024-04-22 17:28:17 +0900

    RAC-339 feat: 인증 화면에서 토큰 만료시 로그인으로 이동

--- src/app/senior/auth/page.tsx
+++ src/app/senior/auth/page.tsx
@@ -29,6 +29,14 @@ function SeniorAuthPage() {
 
   useEffect(() => {
     getAccessToken().then((tkn) => {
+      if(!tkn) { // 알림톡으로 들어와서 토큰 없을 시, 로그인으로 이동
+        const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
+        const REDIRECT_URI = window.location.origin + '/login/oauth2/code/kakao';
+        const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+        window.location.href = link;
+        return;
+      }
+
       setAccessTkn(tkn);
     });
     const userT = getUserType();
