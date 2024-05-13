19fddd232a1e04136fd16d1e821bb57e5db14fbc
leehyewon0531 <ifpffk@naver.com>
2024-05-13 15:35:40 +0900

    RAC-365 feat: 프로필 수정 및 멘토링 페이지 로그인 유도 추가

--- src/app/senior/mentoring/page.tsx
+++ src/app/senior/mentoring/page.tsx
@@ -1,20 +1,36 @@
 'use client';
 import STapBar from '@/components/Bar/TapBar/SeniorTab/STabBar';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import useAuth from '@/hooks/useAuth';
-import DimmedModal from '@/components/Modal/DimmedModal';
 import { createPortal } from 'react-dom';
 import useModal from '@/hooks/useModal';
 import LogoLayer from '@/components/LogoLayer/LogoLayer';
 import MenuBar from '@/components/Bar/MenuBar';
 import SearchModal from '@/components/Modal/SearchModal';
+
 function SeniorMentoringPage() {
   const {
     modal: searchModal,
     modalHandler: searchModalHandler,
     portalElement: searchPortalElement,
   } = useModal('search-portal');
+  const { getAccessToken } = useAuth();
+
+  useEffect(() => {
+    getAccessToken().then((tkn) => {
+      if (!tkn) {
+        // 알림톡으로 들어와서 토큰 없을 시, 로그인으로 이동
+        const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
+        const REDIRECT_URI =
+          window.location.origin + '/login/oauth2/code/kakao';
+        const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+        window.location.href = link;
+        return;
+      }
+    });
+  }, []);
+
   return (
     <div style={{ width: 'inherit', height: 'inherit' }}>
       <LogoLayer modalHandler={searchModalHandler} />
@@ -31,6 +47,7 @@ function SeniorMentoringPage() {
     </div>
   );
 }
+
 const MenuBarWrapper = styled.div`
   position: fixed;
   width: inherit;
