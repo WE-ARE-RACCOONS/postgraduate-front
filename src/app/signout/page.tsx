d2f49874fd201c90d65363b25d23be9fbe7c986d
Kim0426 <706shin1728@naver.com>
2024-08-10 20:44:02 +0900

    feat: 탈퇴페이지 퍼널로 흐름 구현

--- src/app/signout/page.tsx
+++ src/app/signout/page.tsx
@@ -1,3 +1,48 @@
+import { useFunnel } from '@toss/use-funnel';
+import {
+  SIGNOUT_JUNIOR_SELECT,
+  SIGNOUT_REASON_JUNIOR,
+  SIGNOUT_REASON_SENIOR,
+} from '@/app/signout/constant';
+
 export default function SignOut() {
-  return <div></div>;
+  const [SignoutFunnel, setSignoutStep] = useFunnel([
+    'signout_info',
+    'signout_type_select',
+    'signout_reason',
+    'signout_finish',
+  ] as const).withState<{
+    signOutType: keyof typeof SIGNOUT_JUNIOR_SELECT;
+    signoutReason:
+      | keyof typeof SIGNOUT_REASON_JUNIOR
+      | keyof typeof SIGNOUT_REASON_SENIOR;
+    etc?: string;
+  }>({
+    signOutType: 'JUNIOR',
+    signoutReason: 'ETC',
+    etc: '',
+  });
+
+  const handleSignOutTypeChange = (
+    type: keyof typeof SIGNOUT_JUNIOR_SELECT,
+  ) => {};
+
+  return (
+    <main>
+      <SignoutFunnel>
+        <SignoutFunnel.Step name={'signout_info'}>
+          <div>탈퇴하기 정보 </div>
+        </SignoutFunnel.Step>
+        <SignoutFunnel.Step name={'signout_type_select'}>
+          <div>정보 선택</div>
+        </SignoutFunnel.Step>
+        <SignoutFunnel.Step name={'signout_reason'}>
+          <div>탈퇴 이유</div>
+        </SignoutFunnel.Step>
+        <SignoutFunnel.Step name={'signout_finish'}>
+          <div>탈퇴완료</div>
+        </SignoutFunnel.Step>
+      </SignoutFunnel>
+    </main>
+  );
 }
