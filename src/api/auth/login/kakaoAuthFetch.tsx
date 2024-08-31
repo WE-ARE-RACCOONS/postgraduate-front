6afcd6f83a5c262200c0946c74e5e5819d14ff0d
Kim0426 <706shin1728@naver.com>
2024-09-01 00:06:49 +0900

    feat: kakao 로그인 응답 타입정의

---
+++ src/api/auth/login/kakaoAuthFetch.tsx
@@ -0,0 +1,26 @@
+import instance from '@/api/api';
+import { ResponseModel } from '@/api/model';
+
+interface KakaoAuthFetchResponse extends ResponseModel {
+  data: {
+    accessToken: string;
+    accessExpiration: number;
+    refreshToken: string;
+    role: 'USER' | 'ADMIN' | 'SENIOR';
+    isTutorial: boolean;
+    refreshExpiration: number;
+    socialId: string;
+    isDelete: boolean;
+  };
+}
+
+export const kakaoAuthFetch = async ({ code }: { code: string }) => {
+  return await instance.post<KakaoAuthFetchResponse>(
+    window.location.hostname.includes('localhost')
+      ? `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/dev/login/KAKAO`
+      : `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/login/KAKAO`,
+    {
+      code: code,
+    },
+  );
+};
