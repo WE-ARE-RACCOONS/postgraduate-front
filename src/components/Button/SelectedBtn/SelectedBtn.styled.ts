57492b1f356cf1654086a859d9cb54619e100c08
kimhyojung <706shin1728@naver.com>
2024-09-30 21:30:35 +0900

    RAC 439 Refactor: 선배 프로필 수정 RiseUpModal의 닫기 버튼 추가 및 스타일 수정 (#300)
    * refactor: selectform모달 ui 수정
    
    * refactor: 선배 프로필 연구 주제, 분야 모달 UI 수정
    
    * style: 선배 프로필 수정 모달마다 닫기 버튼 추가 및 스타일 수정
    
    * refactor: 상수 문구 변경

--- src/components/Button/SelectedBtn/SelectedBtn.styled.ts
+++ src/components/Button/SelectedBtn/SelectedBtn.styled.ts
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
 export const StyledSelectedBtn = styled.button<{ $selected: boolean }>`
-  width: max-content;
-  height: 2.125rem;
+  min-width: 60px;
+  width: auto;
+  height: 2.25rem;
   margin: 0.25rem 0.5rem 0.25rem 0;
-  padding: 0.3rem 0.75rem;
+  padding: 0.3rem 0.5rem;
   font-size: 16px;
   font-family: Pretendard;
-  border-radius: 8px;
+  border-radius: 4px;
   cursor: pointer;
-  background-color: ${(props) => (props.$selected ? '#2FC4B2' : '#F8F9FA')};
-  color: ${(props) => (props.$selected ? '#fff' : '#ADB5BD')};
-  border: ${(props) => (props.$selected ? 'none' : '1px solid #DEE2E6')};
+  background-color: ${(props) =>
+    props.$selected ? '#2FC4B2' : 'rgba(124, 143, 141, 0.1)'};
+  color: ${(props) => (props.$selected ? '#fff' : '#4C4D4E')};
+  border: ${(props) => (props.$selected ? '1px solid #21B1A0' : '1px #D7D7D7')};
   font-weight: ${(props) => (props.$selected ? '700' : '400')};
 
   #selected-x-btn {
     width: 1rem;
     height: 1rem;
-    margin-left: 0.375rem;
-    margin-bottom: 0.2rem;
+    margin-left: 0.15rem;
+    margin-bottom: 0.15rem;
     cursor: pointer;
     vertical-align: middle;
   }
