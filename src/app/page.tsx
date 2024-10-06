6660a953734ed7dff382519f77800784205f947a
Kim0426 <706shin1728@naver.com>
2024-10-06 16:38:14 +0900

    feat: 초기 페이지 JSON_LD적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,46 @@ export default async function Home() {
       lastPage.data.seniorSearchResponses.length + 1,
   });
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'ItemList',
+    itemListElement: [
+      {
+        '@type': 'Person',
+        position: 1,
+        item: {
+          '@type': 'Person',
+          name: 'Senior 1',
+          description: 'Description of Senior 1',
+        },
+      },
+    ],
+  };
+
+  const seniorData = await queryClient.fetchQuery({
+    queryKey: ['seniorList'],
+    queryFn: () => getSeniorList({ field: 'all', postgradu: 'all', page: 1 }),
+  });
+
+  jsonLd.itemListElement = seniorData.data.seniorSearchResponses.map(
+    (senior, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@type': 'Person',
+        name: senior.nickName,
+        description: [senior.keyword, senior.lab, senior.professor].join(', '),
+        image: senior.profile,
+      },
+    }),
+  );
+
   return (
     <Hydrate state={dehydrate(queryClient)}>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <SeniorList />
     </Hydrate>
   );
