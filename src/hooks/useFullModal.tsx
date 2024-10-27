26473ff3085fc2cace2e4e85e5cfff2c5b92ff0b
kimhyojung <706shin1728@naver.com>
2024-10-27 17:07:54 +0900

    refactor: 뒤로가기시 모든 모달 닫히게 설정

--- src/hooks/useFullModal.tsx
+++ src/hooks/useFullModal.tsx
@@ -1,16 +1,51 @@
 import FullModal from '@/components/Modal/FullModal';
 import { FullModalProps } from '@/types/modal/full';
 import { overlay } from 'overlay-kit';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 
 interface UseFullModalProps extends FullModalProps {
   overlayId?: string;
 }
 const useFullModal = ({ ...props }: Partial<UseFullModalProps>) => {
   const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    window.addEventListener('popstate', () => {
+      overlay.unmountAll();
+    });
+  }, []);
 
+  const openModalAsync = async () => {
+    setIsOpen(true);
+    overlay.openAsync(
+      async ({ unmount }) => {
+        return (
+          <FullModal
+            {...props}
+            modalType={props?.modalType ?? 'best-case'}
+            modalHandler={() => {
+              if (props.modalHandler) {
+                props.modalHandler();
+              }
+              closeModal(unmount);
+            }}
+            cancelModalHandler={() => {
+              if (props.cancelModalHandler) {
+                props.cancelModalHandler();
+              }
+              closeModal(unmount);
+            }}
+          />
+        );
+      },
+      {
+        overlayId: props.overlayId ?? '',
+      },
+    );
+  };
   const openModal = () => {
     setIsOpen(true);
+
     overlay.open(
       ({ unmount }) => {
         return (
@@ -51,7 +86,7 @@ const useFullModal = ({ ...props }: Partial<UseFullModalProps>) => {
     }
   };
 
-  return { openModal, closeModal, toggleModal, isOpen };
+  return { openModal, closeModal, toggleModal, isOpen, openModalAsync };
 };
 
 export default useFullModal;
