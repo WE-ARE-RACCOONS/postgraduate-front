2f6cc1835984b62ac99728bd1727556f1e312f78
kimhyojung <706shin1728@naver.com>
2024-11-05 23:23:53 +0900

    refactor: alert문 제거

--- src/hooks/mutations/useCancelMyMentoring.ts
+++ src/hooks/mutations/useCancelMyMentoring.ts
@@ -3,8 +3,6 @@ import { useMutation } from '@tanstack/react-query';
 export const useCancelMyMentoring = () => {
   return useMutation({
     mutationFn: cancleMentoring,
-    onSuccess: () => {
-      alert('멘토링 취소를 완료하였습니다!');
-    },
+    onSuccess: () => {},
   });
 };
