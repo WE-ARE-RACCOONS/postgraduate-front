8c6680d65b57b3400d26466db2e66e361ddc086f
kimhyojung <706shin1728@naver.com>
2024-11-20 00:00:54 +0900

    refactor: 인스타 링크 제거

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -116,12 +116,6 @@ function Footer() {
             <FooterItemTitle>마케팅 제휴 문의</FooterItemTitle>
             <FooterItemContent>grad.kimseonbae@gmail.com</FooterItemContent>
           </div>
-          <Image
-            src={InstaGram}
-            alt="인스타그램 링크 이미지"
-            width={14}
-            height={14}
-          />
         </FooterContactSection>
       </FooterInfoSection>
 
