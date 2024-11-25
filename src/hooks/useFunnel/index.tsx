3e4441d424450df53aed9ee132a2324ba394b620
kimhyojung <706shin1728@naver.com>
2024-11-25 23:09:53 +0900

    Update src/hooks/useFunnel/index.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/hooks/useFunnel/index.tsx
+++ src/hooks/useFunnel/index.tsx
@@ -90,7 +90,7 @@ function useFunnel<Steps extends StepArray>(
     Object.assign(FunnelComponent, { Step }),
     updateStep,
     prevStep,
-    currentStep ?? '',
+    currentStep ?? options.initialStep,
   ];
 }
 
