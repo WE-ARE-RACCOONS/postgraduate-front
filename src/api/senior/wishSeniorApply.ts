7a12a10083c15a0b87cc75b0bbe530843562b680
kimhyojung <706shin1728@naver.com>
2024-11-25 18:51:21 +0900

    feat: wishSeniorAPply 뮤테이션 훅 구성

--- src/api/senior/wishSeniorApply.ts
+++ src/api/senior/wishSeniorApply.ts
@@ -1,3 +1,6 @@
+import { withOutAuthInstance } from '../api';
+import { ResponseModel } from '../model';
+
 export interface WishSeniorApplyRequest {
   field: string;
   postgradu: string;
@@ -5,3 +8,22 @@ export interface WishSeniorApplyRequest {
   lab: string;
   phoneNumber: string;
 }
+
+interface WishSeniorApplyResponse extends ResponseModel {
+  data: {};
+}
+
+export const wishSeniorApply = async (
+  seniorApplyReq: WishSeniorApplyRequest,
+) => {
+  try {
+    return (
+      await withOutAuthInstance.post<WishSeniorApplyResponse>(
+        '/wish/apply',
+        seniorApplyReq,
+      )
+    ).data;
+  } catch (e) {
+    throw e;
+  }
+};
