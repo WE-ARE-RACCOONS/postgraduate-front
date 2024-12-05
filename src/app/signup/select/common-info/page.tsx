6bfef57b173c11f1aa1cfc349afab68b1dd9458e
kimhyojung <706shin1728@naver.com>
2024-12-05 23:28:12 +0900

    refactor: 후배 회원 시 원하는 선배 알림톡 부분 제거

--- src/app/signup/select/common-info/page.tsx
+++ src/app/signup/select/common-info/page.tsx
@@ -9,20 +9,31 @@ import {
   notDuplicate,
   phoneNum,
   phoneNumValidation,
-  userTypeAtom,
 } from '@/stores/signup';
 import BackHeader from '@/components/Header/BackHeader';
 import { essential } from '@/stores/condition';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { detectReload, preventClose } from '@/utils/reloadFun';
+import { useSignUpUser } from '@/hooks/mutations/useSignupUser';
+import useAuth from '@/hooks/useAuth';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/hooks/useToast';
+import { SignUpBtnContainer } from '@/components/Button/SignUpBtn/SignUpBtn.styled';
 
 function CommonInfoPage() {
-  const userType = useAtomValue(userTypeAtom);
+  const [socialId, setSocialId] = useState<number | null>(null);
+
+  const router = useRouter();
+  const { addToast } = useToast();
+  const { setAccessToken, setRefreshToken, setUserType } = useAuth();
+
   const userNick = useAtomValue(changeNickname);
   const fullNum = useAtomValue(phoneNum);
   const service = useAtomValue(essential);
   const nickAvailable = useAtomValue(notDuplicate);
   const phoneAvailable = useAtomValue(phoneNumValidation);
+
+  const { mutate: signupUser } = useSignUpUser();
 
   useEffect(() => {
     detectReload();
@@ -34,6 +45,47 @@ function CommonInfoPage() {
     };
   }, []);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const socialId = window.localStorage.getItem('socialId');
+      const socialIdNum = socialId ? parseInt(socialId) : null;
+      setSocialId(socialIdNum);
+    }
+  }, []);
+
+  const handleSignup = () => {
+    if (socialId) {
+      signupUser(
+        {
+          socialId,
+          nickName: userNick,
+          phoneNumber: fullNum,
+          marketingReceive: service,
+        },
+        {
+          onError: () => {
+            addToast({
+              message: '회원가입을 할 수 없습니다. 잠시 후 다시 시도해주세요',
+              status: 'error',
+            });
+          },
+          onSuccess: (data) => {
+            setAccessToken({
+              token: data.accessToken,
+              expires: data.accessExpiration,
+            });
+            setRefreshToken({
+              token: data.refreshToken,
+              expires: data.refreshExpiration,
+            });
+            setUserType(data.role);
+            router.push('/signup/done');
+          },
+        },
+      );
+    }
+  };
+
   return (
     <div>
       <div style={{ boxShadow: '0px 4px 8px 0px rgba(0, 0, 0, 0.10)' }}>
@@ -44,11 +96,9 @@ function CommonInfoPage() {
       <PhoneNumForm />
       <ServiceCondition />
       {userNick && fullNum && service && nickAvailable && phoneAvailable ? (
-        <NextBtn
-          kind="next"
-          url={userType == 'junior' ? '/matching-info' : '/senior-info/major'}
-          btnText="다음으로"
-        />
+        <SignUpBtnContainer onClick={handleSignup}>
+          가입완료 하기
+        </SignUpBtnContainer>
       ) : (
         <NextBtn kind="route-non" btnText="다음으로" />
       )}
