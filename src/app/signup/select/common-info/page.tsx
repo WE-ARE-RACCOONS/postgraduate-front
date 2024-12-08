3997ccbbddc4dfa603fb22fc1eec2614804c8b82
kimhyojung <706shin1728@naver.com>
2024-12-08 15:43:18 +0900

    Update page.tsx

--- src/app/signup/select/common-info/page.tsx
+++ src/app/signup/select/common-info/page.tsx
@@ -1,8 +1,9 @@
 'use client';
-import NicknameForm from '@/components/SingleForm/NicknameForm';
-import ServiceCondition from '@/components/ServiceCondition';
-import NextBtn from '@/components/Button/NextBtn';
-import PhoneNumForm from '@/components/SingleForm/PhoneNumForm';
+import NicknameForm from '@/components/Form/NicknameForm';
+import ServiceCondition from './(components)/ServiceCondition';
+import NextBtn from '@/components/common/Button/NextBtn';
+import PhoneNumForm from '@/components/Form/PhoneNumForm';
+
 import { COMMON_INFO_ERROR_MESSAGES } from './(constants)';
 import { useAtomValue } from 'jotai';
 import {
@@ -19,7 +20,7 @@ import { useSignUpUser } from '@/hooks/mutations/useSignupUser';
 import useAuth from '@/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import { useToast } from '@/hooks/useToast';
-import { SignUpBtnContainer } from '@/components/Button/SignUpBtn/SignUpBtn.styled';
+import { SignUpBtnContainer } from '@/components/common/Button/SignUpBtn/SignUpBtn.styled';
 
 function CommonInfoPage() {
   const [socialId, setSocialId] = useState<number | null>(null);
