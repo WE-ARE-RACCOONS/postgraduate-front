a024eaa3c888056df27032f45184ab63b0e49f7a
leehyewon0531 <ifpffk@naver.com>
2024-12-09 17:51:10 +0900

    RAC-470 chore: transform swc로 지정

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': '@swc/jest',
   },
 };
 
