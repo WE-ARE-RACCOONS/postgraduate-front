9cca94c0682bb2bcc46b609e9595423502e006ce
kimhyojung <706shin1728@naver.com>
2024-12-15 21:01:37 +0900

    refactor: 쿠키 이름 통일

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has('refresh_token')) {
+  if (!request.cookies.has('refreshToken')) {
     return NextResponse.redirect(new URL('/', request.url));
   } else {
     return NextResponse.next();
